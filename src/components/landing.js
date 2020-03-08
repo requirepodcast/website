8dbb97703234c275e99f9d08e93762d441aae7a1
datejer <ejer.priv@wp.pl>
2020-03-08 13:41:31 +0100

    Minor tweaks, add player

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,23 +4,20 @@ import styled, { keyframes } from "styled-components"
 import background from "../images/background.png"
 
 const bounce = keyframes`
-	0%, 20%, 50%, 80%, 100% {
+	50% {
+    transform: translateY(-50%);
+  }
+  100% {
     transform: translateY(0);
   }
-  40% {
-    transform: translateY(-30px);
-  }
-  60% {
-    transform: translateY(-15px);
-	}
 `
 
 const Title = styled.h1`
-  margin: 0.25em;
+  margin: 0 0 0.25em 0;
   color: #ff5370;
   font-size: 5em;
   font-weight: 800;
-  @media (max-width: 500px) {
+  @media (max-width: 750px) {
     font-size: 4em;
   }
   @media (max-width: 400px) {
@@ -40,22 +37,39 @@ const Container = styled.div`
   background-size: cover;
   background-position: center;
   flex-direction: column;
+  &::before {
+    content: "╲╱";
+    font-family: Arial;
+    color: #fff;
+    position: absolute;
+    opacity: 0.8;
+    text-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.5);
+    width: 6rem;
+    font-size: 2rem;
+    height: 4rem;
+    line-height: 4rem;
+    text-align: center;
+    bottom: 2rem;
+    margin-left: -3rem;
+    left: 50%;
+    animation: ${bounce} 1s ease infinite;
+    @media (max-height: 550px) {
+      display: none;
+    }
+  }
 `
 
 const Subtitle = styled.h2`
   color: #fff;
-  font-size: 3em;
+  font-size: 1.5em;
   font-weight: 400;
-  margin: 0;
-  @media (max-width: 500px) {
-    font-size: 3em;
+  max-width: 800px;
+  margin: 0px auto;
+  text-align: center;
+  padding: 0 2em;
+  @media (max-width: 750px) {
+    font-size: 1em;
   }
-`
-
-const Arrow = styled.span`
-  margin: 0 1em;
-  animation: 2s ${bounce} infinite;
-  display: inline-block;
 `
 
 const Landing = () => {
@@ -63,9 +77,8 @@ const Landing = () => {
     <Container>
       <Title>The Require Podcast</Title>
       <Subtitle>
-        <Arrow>⯆</Arrow>
-        tune in
-        <Arrow>⯆</Arrow>
+        Przemyślenia dwójki programistów - Adama i Artura - na temat nowości i
+        wydarzeń w świecie frontendu, backendu i nie tylko.
       </Subtitle>
     </Container>
   )
--- src/components/podcast.js
+++ src/components/podcast.js
@@ -12,8 +12,46 @@ const Container = styled.div`
   flex-direction: column;
 `
 
+const Player = styled.iframe`
+  height: 450;
+  max-width: 800px;
+  width: 90%;
+  margin: 3em auto;
+  border: none;
+`
+
+const Heading = styled.h1`
+  margin: 0 0 0.25em 0;
+  color: #ff5370;
+  font-size: 4em;
+  font-weight: 800;
+  @media (max-width: 750px) {
+    font-size: 3em;
+  }
+  @media (max-width: 400px) {
+    font-size: 2em;
+  }
+  &::after {
+    border-top: 2px solid #fff;
+    display: block;
+    width: 33.3%;
+    content: "";
+    margin: 8px auto 0;
+  }
+`
+
 const Podcast = () => {
-  return <Container>siema</Container>
+  return (
+    <Container>
+      <Heading>Posłuchaj</Heading>
+      <Player
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/790825027&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      ></Player>
+    </Container>
+  )
 }
 
 export default Podcast
