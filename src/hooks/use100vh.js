77dd4fd96b20fe2928c12bb8f132e564627136f9
Adam Siekierski <a@siekierski.ml>
2021-01-02 18:13:05 +0100

    RWD fixes

---
+++ src/hooks/use100vh.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+
+export function use100vh() {
+  const [height, setHeight] = useState(measureHeight)
+
+  const wasRenderedOnClientAtLeastOnce = useWasRenderedOnClientAtLeastOnce()
+
+  useEffect(() => {
+    if (!wasRenderedOnClientAtLeastOnce) return
+
+    const updateHeight = () => setHeight(measureHeight())
+    updateHeight()
+
+    window.addEventListener("resize", updateHeight)
+    return () => window.removeEventListener("resize", updateHeight)
+  }, [wasRenderedOnClientAtLeastOnce])
+
+  return wasRenderedOnClientAtLeastOnce ? height : "100vh"
+}
+
+function measureHeight() {
+  if (!isClient()) return "100vh"
+  return document.documentElement?.clientHeight || window.innerHeight
+}
+
+function useWasRenderedOnClientAtLeastOnce() {
+  const [
+    wasRenderedOnClientAtLeastOnce,
+    setWasRenderedOnClientAtLeastOnce,
+  ] = useState(false)
+
+  useEffect(() => {
+    if (isClient()) {
+      setWasRenderedOnClientAtLeastOnce(true)
+    }
+  }, [])
+  return wasRenderedOnClientAtLeastOnce
+}
+
+function isClient() {
+  return typeof window !== "undefined" && typeof document !== "undefined"
+}
