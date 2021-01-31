841b8e35911d83c1e695d096be6910f49428924b
Adam Siekierski <a@siekierski.ml>
2021-01-31 16:47:44 +0100

    playerstate tests (#20)

--- src/hooks/usePlayerState.ts
+++ src/hooks/usePlayerState.ts
@@ -43,6 +43,7 @@ export const usePlayerState = ({
     setPlaying((prev) => {
       prev ? audioRef.current.pause() : audioRef.current.play()
 
+      /* istanbul ignore next */
       if (typeof window !== "undefined" && window.gtag) {
         /* istanbul ignore next  */
         window.gtag("event", prev ? "pause" : "play", {
@@ -58,6 +59,7 @@ export const usePlayerState = ({
   function seek(t: number) {
     const duration = audioRef.current.duration
 
+    /* istanbul ignore else */
     if (duration) {
       audioRef.current.currentTime = time + t
 
@@ -79,6 +81,7 @@ export const usePlayerState = ({
   function handleSliderSeek(e: SyntheticEvent<HTMLDivElement, MouseEvent>) {
     const duration = audioRef.current.duration
 
+    /* istanbul ignore else */
     if (duration) {
       audioRef.current.currentTime =
         (e.nativeEvent.offsetX / sliderRef.current.clientWidth) * duration
